0cde669c11a012b0f97286702ec860f59745c082
Xi Zhang <xzhangcpp@gmail.com>
2015-10-12 17:02:29 -0700

    optimize script code

--- Assets/Scripts/BgManagerScript.cs
+++ Assets/Scripts/BgManagerScript.cs
@@ -18,24 +18,41 @@ public class BgManagerScript : MonoBehaviour {
 
 		groundQ = new Queue<GameObject> (numOfGround);
 
+		//groundPrefab must be an instance which has valid transform position
 		for (int i = 0; i < numOfGround; i++) {
-			GameObject ground = (GameObject)Instantiate (groundPrefab, nextPos, Quaternion.identity);
+			GameObject ground = (GameObject)Instantiate (groundPrefab);
 			groundQ.Enqueue(ground);
-			nextPos += shift;
+		}
+
+		for (int i = 0; i < numOfGround; i++) {
+			recycle(ref groundQ, ref nextPos, shift);
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		GameObject ground = groundQ.Peek ();
-		
-		Vector3 screenPosition = Camera.main.WorldToScreenPoint (ground.transform.position + shift * 0.5f);
-		if (screenPosition.x < 0) {
-			ground = groundQ.Dequeue ();
-			ground.transform.position = nextPos;
-			groundQ.Enqueue (ground);
-			nextPos += shift;
-		}
-		}
 
+		if (outOfScreen(ground.transform.position, shift)) {
+			recycle(ref groundQ, ref nextPos, shift);
+		}
+	}
+
+	// Move the prefab to next position
+	void recycle(ref Queue<GameObject> queue, ref Vector3 nextPos, Vector3 shift) {
+		GameObject obj = queue.Dequeue ();
+		obj.transform.position = nextPos;
+		queue.Enqueue (obj);
+		nextPos += shift;
+	}
+	
+	bool outOfScreen (Vector3 pos, Vector3 scale) {
+		Vector3 screenPosition = Camera.main.WorldToScreenPoint (pos + scale * 0.5f);
+
+		if (!Camera.main) {
+			Debug.LogError("Please set main camera in tag");
+			return false;
+		}
+		return screenPosition.x < 0;
+	}
 }
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,17 +3,16 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	public GameObject player;
+	public Runner player;
 	private float distanceX;
-
-	// Use this for initialization
+	
 	void Start () {
 		distanceX = transform.position.x - player.transform.position.x;
-//		distanceY = transform.position.y - player.transform.position.y;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
+
+		//simple follow
 		transform.position = new Vector3 (player.transform.position.x + distanceX ,transform.position.y, transform.position.z);
 	}
 }
