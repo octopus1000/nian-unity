4ee14568fe45f97985defebe038d0f6cf95403ed
Determ <jxyfwrj@gmail.com>
2015-10-16 16:41:42 -0700

    background far and near effect

--- Assets/Scripts/BgManagerScript.cs
+++ Assets/Scripts/BgManagerScript.cs
@@ -6,35 +6,49 @@ public class BgManagerScript : MonoBehaviour {
 
 	public int numOfGround;
 	public GameObject groundPrefab;
+	public GameObject hill;
 
 	private Queue<GameObject> groundQ;
+	private Queue<GameObject> hillQ;
 	private Vector3 nextPos;
 	private Vector3 shift; 
+	private Vector3 nextPosh;
+	private Vector3 shifth; 
 
 	void Start () {
 		Renderer renderer = groundPrefab.GetComponent<Renderer>();
+		Renderer rendererh = hill.GetComponent<Renderer> ();
 		shift = new Vector3(renderer.bounds.size.x,0,0);
+		shifth = new Vector3(rendererh.bounds.size.x, 0,0);
 		nextPos = groundPrefab.transform.position;
-
+		nextPosh = hill.transform.position;
 		groundQ = new Queue<GameObject> (numOfGround);
+		hillQ = new Queue<GameObject> (numOfGround);
 
 		//groundPrefab must be an instance which has valid transform position
 		for (int i = 0; i < numOfGround; i++) {
 			GameObject ground = (GameObject)Instantiate (groundPrefab);
+			GameObject newhill = (GameObject)Instantiate (hill);
 			groundQ.Enqueue(ground);
+			hillQ.Enqueue(newhill);
 		}
 
 		for (int i = 0; i < numOfGround; i++) {
 			recycle(ref groundQ, ref nextPos, shift);
+			recycle(ref hillQ, ref nextPosh, shifth);
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		GameObject ground = groundQ.Peek ();
+		GameObject newhill = hillQ.Peek ();
 
 		if (outOfScreen(ground.transform.position, shift)) {
 			recycle(ref groundQ, ref nextPos, shift);
+		}
+		if (outOfScreen(newhill.transform.position, shifth)) {
+			recycle(ref hillQ, ref nextPosh, shifth);
 		}
 	}
 
