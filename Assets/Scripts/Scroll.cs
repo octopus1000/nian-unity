198d8b846c4522d560202c6e70f21530d698c09f
RichardLee6647 <liyangusc.cs@gmail.com>
2015-10-22 12:13:21 -0700

    missing prefab

--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -9,19 +9,20 @@ public class Scroll : MonoBehaviour {
 	
 	private Vector2 offset;
 	private Renderer rend;
-	
+
 	void Start ()
 	{
 		playerBody = godplayer.GetComponent<Rigidbody2D> ();
 		rend = GetComponent<Renderer> ();
 		distanceX = transform.position.x - godplayer.transform.position.x;
-
 	}
 	
 	void FixedUpdate ()
 	{
 		offset.x = Time.time * playerBody.velocity.x / 700;
-		rend.material.SetTextureOffset("_MainTex", offset);
+		if (rend != null)
+			rend.material.SetTextureOffset("_MainTex", offset);
+
 		transform.position = new Vector3 (godplayer.transform.position.x + distanceX, transform.position.y, transform.position.z);
 	}
 }
