786407db50becffd8c63c3d873c2e7ae1a358325
Daniel Lu <dalu@usc.edu>
2015-10-22 18:08:14 -0700

    minimap & life

---
+++ Assets/Minimap.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Minimap : MonoBehaviour {
+
+	public GameObject player;
+	public float distance = 1000;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		GetComponent<Slider> ().value = (float)(player.transform.position.x + 8.16)/distance;
+	}
+}
--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -6,12 +6,19 @@ public class UIManagerScript : MonoBehaviour {
 	public Text coinTxt;
 	public Button upBtn, attackBtn;
 	public Text pauseBtnTxt;
-	public Text lifeTxt;
+	public GameObject[] life_objects;
 	public Canvas canvas;
 	public Runner runner;
+	public Sprite life_full;
+	public Sprite life_half;
+	public Sprite life_frame;
 
 	void Start () {
 		canvas.enabled = false;
+		life_objects = new GameObject[5];
+		for (int j = 0; j < 5; j++) {
+			life_objects[j] = GameObject.FindGameObjectWithTag("Life" + j);	
+		}
 	}
 
 	void Update() {
@@ -26,7 +33,37 @@ public class UIManagerScript : MonoBehaviour {
 	}
 
 	public int updateLife (int life) {
-		lifeTxt.text = "Life:" + life;
+//		life_objects = GameObject.FindGameObjectsWithTag ("Life");
+		Image image;
+		int i;
+		Debug.Log (life_objects[0].name+" "+life_objects[1].name+" "+life_objects[2].name+" "+life_objects[3].name+" "+life_objects[4].name+" ");
+		if (life % 2 == 0) {
+			for (i = 0; i < life/2; i++) {
+				image = life_objects [i].GetComponent<Image> ();
+				image.sprite = life_full;
+			}
+
+			for (i = life/2; i < 5; i++) {
+				image = life_objects [i].GetComponent<Image> ();
+				image.sprite = life_frame;
+			}
+		} 
+		else {
+			for (i = 0; i < (life-1)/2; i++) {
+				image = life_objects [i].GetComponent<Image> ();
+				image.sprite = life_full;
+			}
+
+			image = life_objects[(life-1)/2].GetComponent<Image>();
+			image.sprite = life_half;
+
+			for (i = (life+1)/2; i < 5; i++) {
+				image = life_objects [i].GetComponent<Image> ();
+				image.sprite = life_frame;
+			}
+		}
+
+//		lifeTxt.text = "Life:" + life;
 		return 0;
 	}
 
