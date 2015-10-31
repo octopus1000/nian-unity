350632f55b9b1a62214cff54d02db2189f945449
Daniel Lu <dalu@usc.edu>
2015-10-30 17:39:45 -0700

    UI

---
+++ Assets/Play_Pop.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play_Pop : MonoBehaviour {
+
+	public GameObject Play_panel;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	}
+
+	public void pop() {
+		InvokeRepeating("moveLeft", 0.1f,0.01f);
+	}
+
+	public void shrink() {
+		InvokeRepeating("moveRight", 0.1f,0.01f);
+	}
+
+	public void moveLeft() {
+		if (this.transform.position.x > -230) {
+			this.transform.position = new Vector2 (this.transform.position.x - 10, this.transform.position.y);
+			Play_panel.transform.position = new Vector2 (Play_panel.transform.position.x - 10, Play_panel.transform.position.y);
+		} else {
+			CancelInvoke();
+		}
+	}
+
+	public void moveRight() {
+		if (this.transform.position.x < 240) {
+			this.transform.position = new Vector2 (this.transform.position.x + 10, this.transform.position.y);
+			Play_panel.transform.position = new Vector2 (Play_panel.transform.position.x + 10, Play_panel.transform.position.y);
+		} else {
+			CancelInvoke();
+		}
+	}
+}
---
+++ Assets/Setting_Panel.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Setting_Panel : MonoBehaviour {
+
+	public GameObject Setting_panel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void pop() {
+		InvokeRepeating("moveLeft", 0.1f,0.01f);
+	}
+	
+	public void shrink() {
+		InvokeRepeating("moveRight", 0.1f,0.01f);
+	}
+	
+	public void moveLeft() {
+		if (this.transform.position.x > -230) {
+			this.transform.position = new Vector2 (this.transform.position.x - 10, this.transform.position.y);
+			Setting_panel.transform.position = new Vector2 (Setting_panel.transform.position.x - 10, Setting_panel.transform.position.y);
+		} else {
+			CancelInvoke();
+		}
+	}
+	
+	public void moveRight() {
+		if (this.transform.position.x < 240) {
+			this.transform.position = new Vector2 (this.transform.position.x + 10, this.transform.position.y);
+			Setting_panel.transform.position = new Vector2 (Setting_panel.transform.position.x + 10, Setting_panel.transform.position.y);
+		} else {
+			CancelInvoke();
+		}
+	}
+}
